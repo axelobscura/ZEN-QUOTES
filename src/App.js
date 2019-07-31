34b04901c48e0200425df039edd2f54c9ca9f724
Axel <axosar@gmail.com>
2019-07-31 18:28:29 -0500

    ZEN UOTES
    ZEN DEVELOPER QUOTES

--- src/App.js
+++ src/App.js
@@ -1,26 +1,41 @@
-import React from 'react';
+import React, {Component} from 'react';
 import logo from './logo.svg';
 import './App.css';
+import axios from "axios";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends Component {
+  constructor(props){
+    super(props);
+    this.state = {
+      quote: ''
+    }
+  }
+  componentDidMount(){
+    axios.get("https://api.github.com/zen").then(response => {
+      console.log(response);
+      this.setState({ quote: response.data });
+    })
+  }
+  render(){
+    return (
+      <div className="App">
+        <header className="App-header">
+          <img src={logo} className="App-logo" alt="logo" />
+          <p>
+            ZEN DEVELOPER QUOTES
+          </p>
+          <a
+            className="App-link"
+            href="https://reactjs.org"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            {this.state.quote}
+          </a>
+        </header>
+      </div>
+    );
+  }
 }
 
 export default App;
